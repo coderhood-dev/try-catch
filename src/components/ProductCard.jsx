27c446f5f042ac2fbe7b45eede2576e4d7ceb42d
BautistaDiSanto <juanbautistadisanto@gmail.com>
2020-12-05 15:29:39 -0300

    CardFix

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,86 @@
-import { Flex, Text, Image, Box } from "@chakra-ui/react"
+import { Flex, Text, Image, Box, Button } from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
 
-export const ProductCard = () =>{
-    return(
-        <Box overflow='hidden' justifyContent='center' align='center' direction='column'  m='1rem' w='16rem' h='16rem'
-        _hover={{cursor:'pointer'}}>
-            <Flex h='75%' justify='center' align='center' overflow='hidden'>
-                <Image src="/PlaceHolder.jpg" alt="imagen no disponible" w='100%' h='100%' _hover={{transition:'1s', transform: 'scale(1.1)' }} />
-            </Flex>
-            <Flex paddingLeft='0.5rem' direction='column' h='25%' w='100%' justifyContent='flex-start' alignItems='flex-start'>
-                    <Text>Ps5 digital edition</Text>
-                <Flex direction='row' align='center' width='100%' marginTop='-0.5rem' justify='space-between'>
-                    <Text fontSize="xl" color='#ad172b'>$200.000</Text>
-                    <Flex as='button' justify='center' align='center' mr='1rem' w='1.5rem' h='1.5rem' borderRadius='50%' bgColor='black' color='white' _hover={{transform:'scale(1.1)'}}> 
-                    +
-                    </Flex>
-                </Flex>
-            </Flex>
-        </Box>
-    )
-}
+export const ProductCard = ({ image, title, price }) => {
+  return (
+    <Box
+      overflow="hidden"
+      justifyContent="center"
+      align="center"
+      direction="column"
+      m="1rem"
+      w="16rem"
+      h="16rem"
+      borderRadius="7px"
+      bgColor="white"
+      border="1px solid #ededed"
+      _hover={{ transition: "1s", cursor: "pointer", boxShadow: "lg" }}
+    >
+      <Button
+        _hover={{ transform: "scale(1.1)", bgColor: "black" }}
+        size="sm"
+        borderRadius="100rem"
+        bgColor="black"
+        color="white"
+        pos="fixed"
+        ml="5rem"
+        mt="0.7rem"
+        zIndex={10}
+      >
+        <Search2Icon w={2} h={2} />
+      </Button>
+
+      <Flex h="75%" justify="center" align="center" overflow="hidden">
+        <Image
+          src="/ps5.jpg"
+          alt="imagen no disponible"
+          w="100%"
+          h="100%"
+          _hover={{ transition: "0.7s", transform: "scale(1.1)" }}
+        />
+      </Flex>
+
+      <Flex
+        paddingLeft="0.5rem"
+        direction="column"
+        h="25%"
+        w="100%"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        borderTop="1px solid #ededed"
+      >
+        <Flex h="50%">
+          <Text fontSize="xl" isTruncated="true">
+            Ps5 Digital edition
+          </Text>
+        </Flex>
+
+        <Flex
+          direction="row"
+          align="center"
+          justify="space-evenly"
+          width="100%"
+          height="50%"
+        >
+          <Flex justify="flex-start" width="50%">
+            <Text color="#ad172b">$200.000</Text>
+          </Flex>
+
+          <Flex width="50%" justify="flex-end">
+            <Button
+              mr="0.9rem"
+              size="sm"
+              mb="0.5rem"
+              borderRadius="100rem"
+              bgColor="black"
+              color="white"
+              _hover={{ transform: "scale(1.1)" }}
+            >
+              +
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
