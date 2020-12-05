5caac5aec248b1c957318e00a472c4afca825e47
unknown <quiramapino347@gmail.com>
2020-12-05 14:44:19 -0500

    adding branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import SliderHome from "./Components/SliderHome"
 
 function App() {
   return (
     <div className="App">
+      <SliderHome/>
       <h1>Try Catch para el mundo!</h1>
     </div>
   );
---
+++ src/Components/SliderHome.jsx
@@ -0,0 +1,8 @@
+
+export const SliderHome = ()=>{
+    return(
+        <div>
+            
+        </div>
+    )
+} 
