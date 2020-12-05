a89ea4dd975932fda51bd530cd6229b3010b1874
BautistaDiSanto <juanbautistadisanto@gmail.com>
2020-12-05 15:38:02 -0300

    CardFix and Chakraicons integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import { ProductCard } from "./Components/ProductCard"
-
-
 function App() {
   return (
     <div className="App">
       <h1>Try Catch para el mundo!</h1>
-      <ProductCard/>
+      <ProductCard />
     </div>
   );
 }
