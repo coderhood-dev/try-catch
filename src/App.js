d6ad0ef7562422fb9f2143f844fd165588075be8
unknown <quiramapino347@gmail.com>
2020-12-06 09:40:24 -0500

    configutation from app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import SliderHome from "./Components/SliderHome"
-// import Slider1 from "./Components/Slider1"
-
 function App() {
   return (
     <div className="App">
-      <SliderHome/>
       <h1>Try Catch para el mundo!</h1>
     </div>
   );
