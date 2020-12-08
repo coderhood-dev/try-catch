802130869cf2be472e1f49eb6f2c57a8194840b7
unknown <quiramapino347@gmail.com>
2020-12-08 15:50:20 -0500

    adding branch and styles with product card

---
+++ src/Components/SliderSection.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+// import logo from "../Images/logo.jpg";
+import { ProductCard } from "./ProductCard";
+import { Flex, Box } from "@chakra-ui/react";
+// Import Swiper styles
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
+
+// install Swiper components
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+const data = [
+  {
+    id: 1,
+    username: "shadrack",
+  },
+  {
+    id: 2,
+    username: "dennis",
+  },
+  {
+    id: 3,
+    username: "shadrack",
+  },
+];
+
+export const SliderSection = () => {
+  return (
+    <Flex
+      width="50%"
+      align="center"
+      justify="center"
+      display="block"
+      margin="auto"
+    >
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={3}
+        navigation
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={(swiper) => console.log("slide change")}
+      >
+        {data.map((user) => (
+          <SwiperSlide key={user.id}>
+            <Box mr="2rem" ml="2rem" textAlign="center">
+              {user.testimonial}
+              <ProductCard />
+            </Box>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Flex>
+  );
+};
