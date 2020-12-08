8fe4ad4d1663a55508577b93d2542077e96ded64
jhonatanquirama <quiramapino47@gmail.com>
2020-12-08 11:50:39 -0500

    quit component from app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import {DetailScreen} from "./components/DetailScreen"
-
 function App() {
   return (
     <div className="App">
-      <DetailScreen/>
       <h1>Try Catch para el mundo!</h1>
     </div>
   );
