752c87160daf4188d958715b89676e4818f61649
Jhonatan Quirama Pino <50423635+kira4489@users.noreply.github.com>
2020-12-10 20:03:11 -0500

    Update SliderSection.jsx
    adding configuration

--- src/Components/SliderSection.jsx
+++ src/Components/SliderSection.jsx
@@ -1,58 +1,69 @@
 import React from "react";
-import SwiperCore, { Navigation, Pagination, A11y } from "swiper";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Box, Image,Center,Flex } from "@chakra-ui/react";
+import { ProductCard } from "./ProductCard";
+import { getProductByCategory } from "../Services";
+import { Flex, Box, Text } from "@chakra-ui/react";
+// Import Swiper styles
 import "swiper/swiper.scss";
-import logo from "../Images/logo.png";
 import "swiper/components/navigation/navigation.scss";
 import "swiper/components/pagination/pagination.scss";
 import "swiper/components/scrollbar/scrollbar.scss";
 
-SwiperCore.use([Navigation, Pagination, A11y]);
+// install Swiper components
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
-const data = [
-  {
-    id: 1,
-  },
-  {
-    id: 2,
-  },
-  {
-    id: 3,
-  },
-  {
-    id: 4,
-  },
-  {
-    id: 5,
-  },
-  {
-    id: 6,
-  },
-];
+export const SliderSection = ({ titleSection, idCategory }) => {
+  const [products, setProducts] = React.useState([]);
+  console.log("hola")
+  React.useEffect(() => {
+    console.log("hola usefefect")
+    const doGetCategoryProducts = async () => {
+      try {
+        const data = await getProductByCategory(idCategory);
+        // console.log([data1.data.items, data2.data.items, data3.data.items])
+        console.log(data)
+        setProducts(data);
+        
+      } catch (e) {
+        console.log(e);
+        setProducts([]);
+      }
+    };
+    doGetCategoryProducts();
+  }, []);
 
-export const SliderSection=()=> {
   return (
-    <Flex width="100vh" align="center" justify="center" display="block" margin="auto">
-      <Swiper
-        spaceBetween={20}
-        slidesPerView={3}
-        navigation
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={(swiper) => console.log("slide change")}
-      >
-        {data.map((user) => (
-          <SwiperSlide key={user.id}>
-            <Box mr="2rem" ml="2rem" textAlign="center">
-              {user.testimonial}
-              <img width="100%" height="100%"
-              src={logo} />
-            </Box>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <Flex
+      width="50%"
+      h="100vh"
+      align="center"
+      justify="center"
+      display="block"
+      margin="auto"
+      flexDirection="column"
+    >
+      <Flex>
+        <Text>{titleSection}</Text>
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={3}
+          navigation
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          // onSlideChange={(swiper) => console.log("slide change")}
+        >
+          {["1","2","3","4","5"].map((product, index) => (
+            <SwiperSlide key={index}>
+              <Box mr="2rem" ml="2rem" textAlign="center">
+                {/* <ProductCard key={index} data={product} /> */}
+                {product}
+              </Box>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Flex>
     </Flex>
   );
-}
-
+};
